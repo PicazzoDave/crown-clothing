ceb9ed786b9bdfe472d4cd7a8e61ec2a3f4a9619
PicazzoDave <david@picazzodata.com>
2023-11-21 16:30:23 +0800

    Refactored user context to use reduce

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,32 +1,54 @@
-import { createContext, useState, useEffect } from 'react';
+import { createContext, useEffect, useReducer } from 'react';
 
-import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase.utils';
+import {
+  onAuthStateChangedListener,
+  createUserDocumentFromAuth,
+} from '../utils/firebase/firebase.utils';
 
-// as the actual value you want to access
 export const UserContext = createContext({
-    currentUser: () => null,
-    setCurrentUser: () => null,
+  setCurrentUser: () => null,
+  currentUser: null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER',
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return { ...state, currentUser: payload };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
 export const UserProvider = ({ children }) => {
-    const [currentUser, setCurrentUser] = useState(null);
-    const value = { currentUser, setCurrentUser };
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
 
-    useEffect(() => {
+  const setCurrentUser = (user) =>
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, currentUser: user });
 
-        const unsubscribe = onAuthStateChangedListener((user) => {
-            
-            //Only create a user docuemnt if a user comes through
-            if (user) {
-                createUserDocumentFromAuth(user);
-            }
-            //When null, the user is signed out. When user successfully authenticated, return user object.
-            setCurrentUser(user);
-        });
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      setCurrentUser(user);
+    });
 
-        //Clean up the Listner methoed
-        return unsubscribe; 
-    }, []);
-    
-    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
-}
+    return unsubscribe;
+  }, []);
+
+  const value = {
+    currentUser,
+  };
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
