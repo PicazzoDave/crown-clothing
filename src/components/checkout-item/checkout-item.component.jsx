929d07440b86de7e8b0e5538d6071fc704574f42
PicazzoDave <david@picazzodata.com>
2023-11-23 16:19:43 +0800

    Refactored context files with Redux.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,5 +1,8 @@
-import { useContext } from 'react';
-import { CartContext } from '../../context/cart.context';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { selectCartItems } from '../../store/cart/cart.selector';
+import { addItemToCart, removeItemFromCart, clearItemFromCart } from '../../store/cart/cart.action';
+
 
 import {
     CheckoutItemContainer,
@@ -12,13 +15,15 @@ import {
   } from './checkout-item.styles';
 
 const CheckoutItem = ( {cartItem} ) => {
+    
     const { name, imageUrl, price, quantity } = cartItem;
+    const cartItems = useSelector(selectCartItems); 
 
-    const { removeItemFromCart, addItemToCart, clearItemFromCart } = useContext(CartContext);
+    const dispatch = useDispatch();
 
-    const removeItemHandler = () => removeItemFromCart(cartItem);
-    const addItemHandler = () => addItemToCart(cartItem);
-    const clearItemHandler = () => clearItemFromCart(cartItem);
+    const removeItemHandler = () => dispatch(removeItemFromCart(cartItems, cartItem));
+    const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem));
+    const clearItemHandler = () => dispatch(clearItemFromCart(cartItems, cartItem));
 
     return (
         <CheckoutItemContainer>
--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,15 +1,18 @@
-import { useContext } from 'react';
-import { CartContext } from '../../context/cart.context';
+import { useDispatch, useSelector } from 'react-redux';
 
-import Button from '../button/button.component';
+import { selectCartItems } from '../../store/cart/cart.selector';
+import { addItemToCart } from '../../store/cart/cart.action';
+
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
 import { ProductCardContainer } from './product-card.styles';
 
 const ProductCard = ( { product }) => {
     const { name, price, imageUrl } = product;
-    const { addItemToCart } = useContext(CartContext);
+    const dispatch = useDispatch();
+    const cartItems = useSelector(selectCartItems);
 
-    const addProductToCart = () => addItemToCart(product);
+    const addProductToCart = () => dispatch(addItemToCart(cartItems, product));
 
     return (
         <ProductCardContainer>
@@ -18,7 +21,7 @@ const ProductCard = ( { product }) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType='inverted' onClick={addProductToCart}>Add to Cart</Button>
+            <Button buttonType={BUTTON_TYPE_CLASSES.inverted} onClick={addProductToCart}>Add to Cart</Button>
         </ProductCardContainer>
     )
 }
