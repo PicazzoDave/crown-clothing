b20e02163aa071381a6dde0505fd66f206f809d8
PicazzoDave <david@picazzodata.com>
2023-11-24 08:03:02 +0800

    Added Redux-Thunk Middleware Logic

--- src/store/categories/categories.action.js
+++ src/store/categories/categories.action.js
@@ -1,7 +1,32 @@
 import CATEGORIES_ACTION_TYPES from './categories.types';
-
 // export const createAction = (type, payload) => ({ type, payload });
 import { createAction } from '../../utils/reducer/reducer.utils';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
-export const setCategories = (categoriesArray) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+export const fetchCategoriesStart = () => 
+    createAction(
+        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START
+    );
+
+export const FETCH_CATEGORIES_SUCCESS = (categoriesArray) => 
+    createAction(
+        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, 
+        categoriesArray
+    );
+
+export const FETCH_CATEGORIES_FAILED = (error) => 
+    createAction(
+        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, 
+        error
+    );
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+
+    try {
+        const categoriesArray = await getCategoriesAndDocuments('categories');
+        dispatch(FETCH_CATEGORIES_SUCCESS(categoriesArray));
+    } catch (error) {
+        dispatch(FETCH_CATEGORIES_FAILED(error));
+    }
+}
