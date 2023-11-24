04fb3de484d95196f6763e69dd5c53129dc45357
PicazzoDave <david@picazzodata.com>
2023-11-24 14:41:21 +0800

    Refactored Redux-Thunk Middleware with Redux-Saga Middleware for the cateogries pattern.

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom';
 import ProductCard from '../../components/product-card/product-card.component';
 import Spinner from '../../components/spinner/spinner.component';
 
-import { selectCategoriesMap, selectCategoriesIsLoading } from '../../store/categories/categories.selector';
+import { selectCategoriesMap, selectCategoriesIsLoading } from '../../store/categories/category.selector';
 
 import { CategoryContainer, Title } from './category.styles';
 
